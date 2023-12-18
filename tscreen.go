a2760670e71d07381beb42755c8ac440010d811b
ernestrc <ernest@unstable.build>
2023-12-18 08:47:43 -0800

    Fix deadlock on quit

--- tscreen.go
+++ tscreen.go
@@ -1754,7 +1754,10 @@ func (t *tScreen) inputLoop(stopQ chan struct{}) {
 			return
 		}
 		if n > 0 {
-			t.keychan <- chunk[:n]
+			select {
+			case t.keychan <- chunk[:n]:
+			case <-t.quit:
+			}
 		}
 	}
 }
