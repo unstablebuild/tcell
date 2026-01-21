86d9894563909fbaa004bceb0ee28d2e82de964b
ernestrc <ernest@unstable.build>
2026-01-21 11:27:46 +0100

    Update AttrMask to be a uint8
    Saves quit a bit of space when used across thousands of cells

--- attr.go
+++ attr.go
@@ -16,7 +16,7 @@ package tcell
 
 // AttrMask represents a mask of text attributes, apart from color.
 // Note that support for attributes may vary widely across terminals.
-type AttrMask int
+type AttrMask uint8
 
 // Attributes are not colors, but affect the display of text.  They can
 // be combined.
